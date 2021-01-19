3c9cecaca30f478df52353f6a7f8132cdb2305c9
Mohankumar <mohankumar.nagarajan@abbvie.com>
2021-01-19 11:09:46 +0530

    Initial commit

---
+++ FormAndCoreData/AddGarmentViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddGarmentViewController.swift
+//  FormAndCoreData
+//
+//  Created by N., Mohankumar (Cognizant) on 19/01/21.
+//
+
+import UIKit
+
+protocol AddGarmentDelegate {
+    func garmentAdded()
+}
+
+class AddGarmentViewController: UIViewController {
+
+    @IBOutlet weak var garmentNameTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var garmentNameLabel: UILabel!
+    var delegate : AddGarmentDelegate?
+    var garmentViewModel : GarmentViewModel = GarmentViewModel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationBar.topItem?.title = NSLocalizedString("Add", comment: "")
+        self.saveButton.title = NSLocalizedString("Save", comment: "")
+        self.garmentNameLabel.text = NSLocalizedString("Garment name", comment: "")
+        self.garmentNameTextField.placeholder = NSLocalizedString("Enter name", comment: "")
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveGarment(_ sender: Any) {
+        if let name  = garmentNameTextField.text{
+            garmentViewModel.currentGarmentName = name
+            if garmentViewModel.isGarmentNameValid(){
+                garmentViewModel.addGarment()
+                self.delegate?.garmentAdded()
+                self.dismiss(animated: true, completion: nil)
+            }
+            else{
+                let alert = UIAlertController.init(title: "Error", message: garmentViewModel.errorMessage, preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "Ok", style: .cancel, handler: nil))
+                present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FormAndCoreData/GarmentListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  FormAndCoreData
+//
+//  Created by N., Mohankumar (Cognizant) on 19/01/21.
+//
+
+import UIKit
+
+class GarmentListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var garmentViewModel : GarmentViewModel = GarmentViewModel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = NSLocalizedString("List", comment: "")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        garmentViewModel.refreshData()
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "add"{
+            let add = segue.destination as! AddGarmentViewController
+            add.delegate = self
+        }
+    }
+    
+    @IBAction func sort(_ sender: UISegmentedControl) {
+        garmentViewModel.selection = sender.titleForSegment(at: sender.selectedSegmentIndex)!
+        self.tableView.reloadData()
+    }
+    
+}
+
+extension GarmentListViewController : UITableViewDelegate,UITableViewDataSource,AddGarmentDelegate{
+    
+    func garmentAdded() {
+        garmentViewModel.refreshData()
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return garmentViewModel.garments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let garment = garmentViewModel.garments[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "garmentCell")
+        cell?.textLabel?.text = garment.name
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    internal func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.garmentViewModel.deleteGarments(offsets: IndexSet.init(integer: indexPath.row))
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
